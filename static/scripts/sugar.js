89133ce5d435d1137f000f337cef67fa2f77c0d0
Thomas <thomas.keunebroek@gmail.com>
2013-06-09 01:21:06 +0200

    Fix indentation

--- static/scripts/sugar.js
+++ static/scripts/sugar.js
@@ -3,30 +3,30 @@
 */
 
 $(".jump-top").click(function() {
-	$('html:not(:animated),body:not(:animated)').animate({scrollTop: 0});
+  $('html:not(:animated),body:not(:animated)').animate({scrollTop: 0});
 	return false;
 });
 
 $('a[rel=tooltip]').tooltip({
-   	'placement': 'bottom'
+  'placement': 'bottom'
 });
 
 $(".obfuscated-link").each(function() {
-	var that = $(this);
-  	var w = that.attr("href");
-  	that.attr("href", "mailto:" + w.replace(/ /, ".").replace(/ /, "@").replace(/ /, '.'));
+  var that = $(this);
+  var w = that.attr("href");
+  that.attr("href", "mailto:" + w.replace(/ /, ".").replace(/ /, "@").replace(/ /, '.'));
 });
 
 $(".obfuscated-text").each(function() {
-	var that = $(this);
-  	var w = that.text();
-  	that.text(w.replace(/ /, ".").replace(/ /, "@").replace(/ /, '.'));
+  var that = $(this);
+  var w = that.text();
+  that.text(w.replace(/ /, ".").replace(/ /, "@").replace(/ /, '.'));
 });
 
 $(".obfuscated-all").each(function() {
-	var that = $(this);
-  	var w = that.text();
-  	var deObfuscatedText = w.replace(/ /, ".").replace(/ /, "@").replace(/ /, '.');
-  	that.text(deObfuscatedText);
-  	that.attr("href", "mailto:" + deObfuscatedText);
+  var that = $(this);
+  var w = that.text();
+  var deObfuscatedText = w.replace(/ /, ".").replace(/ /, "@").replace(/ /, '.');
+  that.text(deObfuscatedText);
+  that.attr("href", "mailto:" + deObfuscatedText);
 });
